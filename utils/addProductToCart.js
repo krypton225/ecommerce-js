722eedb041af653efc206e028791555f2380f7a9
Krypton <k.mohamed.11.98@gmail.com>
2022-12-30 22:32:03 +0200

    ADD product in show-cart after selected

--- utils/addProductToCart.js
+++ utils/addProductToCart.js
@@ -2,6 +2,8 @@ import ProductOperations from "./productOperations.js";
 
 const CartProduct = (function () {
     const cartIcon = document.getElementById("cart-number");
+    const mainCart = document.getElementById("cart-show-products");
+
     const productsInCart = [];
 
     const main = function () {
@@ -26,10 +28,35 @@ const CartProduct = (function () {
     function addProductIntoCart(product) {
         productsInCart.push(product);
         setLengthAtIconCart();
+        renderProductsAtCart(product[0]);
     }
 
     function setLengthAtIconCart() {
         cartIcon.innerHTML = productsInCart.length;
+    }
+
+    function renderProductsAtCart(product) {
+        const { id, pathPic, picAltText, productPrice } = product;
+
+        mainCart.innerHTML +=
+            `<div id="product-94" class="product-card-home w-full px-3 relative flex justify-between items-center">
+                <img src="${pathPic}" id="product-img--cart-${id}" class="w-[24%] object-cover"
+                alt="${picAltText}" width="20%" height="20%" loading="lazy" draggable="false">
+
+                <h3 id="product-title-333333333" class="text-lg text-center">${picAltText}</h3>
+
+                <div class="flex flex-col items-center">
+                    <span id="product-price-11111111"
+                        class="inline-block rounded-lg py-1 px-3 bg-slate-600 text-white z-50">
+                        Price: $${productPrice}
+                    </span>
+
+                    <div class="w-full px-0 mt-5 flex justify-between items-center">
+                        <button class="btn-custom-2 decrease-item">-</button>
+                        <span class="inline-block mx-[10%] text-lg font-semibold product-counting">1</span>
+                        <button class="btn-custom-2 increase-item">+</button>
+                    </div>
+            </div>`;
     }
 
     return {
