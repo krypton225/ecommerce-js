941db6daaea10306308dab75dc02240cc9fdee74
Krypton <k.mohamed.11.98@gmail.com>
2022-12-31 23:44:28 +0200

    ADD funcs for add total price & calc operations in product

--- utils/addProductToCart.js
+++ utils/addProductToCart.js
@@ -4,6 +4,8 @@ const CartProduct = (function () {
     const cartIcon = document.getElementById("cart-number");
     const mainCart = document.getElementById("cart-show-products");
 
+    let countFinalResult = 0;
+
     const productsInCart = [];
 
     const main = function () {
@@ -21,12 +23,14 @@ const CartProduct = (function () {
 
                 cartIcon.classList.add("w-1", "h-1", "p-3", "rounded-full", "flex", "justify-center", "items-center",
                     "absolute", "-top-3", "left-0", "bg-slate-700", "text-white", "text-base");
+
+                setTotalPrice();
             });
         });
     }
 
     function addProductIntoCart(product) {
-        productsInCart.push(product);
+        productsInCart.push(product[0]);
         setLengthAtIconCart();
         renderProductsAtCart(product[0]);
     }
@@ -39,53 +43,91 @@ const CartProduct = (function () {
         const { id, pathPic, picAltText, productPrice } = product;
 
         mainCart.innerHTML +=
-            `<div id="product-94" class="product-card-home w-full px-3 relative flex justify-between items-center">
-                <span class="block rounded-lg px-4 py-1 absolute top-[-26.4%] left-0 bg-slate-600 text-white z-50">Total: $${productPrice}</span>
-
-                <img src="${pathPic}" id="product-img--cart-${id}" class="w-[24%] object-cover"
+            `<div id="product-cart-${id}" class="product-card-home w-full px-3 relative flex justify-between items-center cart-product-container">
+                <img src="${pathPic}" id="product-img-cart-${id}" class="w-[24%] object-cover"
                 alt="${picAltText}" width="20%" height="20%" loading="lazy" draggable="false">
 
-                <h3 id="product-title-333333333" class="text-lg text-center">${picAltText}</h3>
+                <h3 id="product-title-cart-${id}" class="text-lg text-center">${picAltText}</h3>
 
                 <div class="flex flex-col items-center">
-                    <span id="product-price-11111111" class="inline-block rounded-lg py-1 px-3 bg-slate-600 text-white z-50">
+                    <span id="product-price-cart-${id}" class="inline-block rounded-lg py-1 px-3 bg-slate-600 text-white z-50">
                         Price: $${productPrice}
                     </span>
 
                     <div class="w-full px-0 mt-5 flex justify-between items-center product-operations">
-                        <button class="btn-custom-2 decrease-cart-item">-</button>
+                        <button class="btn-custom-2 decrease-cart-item-btn" disabled>-</button>
                         <span class="inline-block mx-[10%] text-xl font-semibold product-counting">1</span>
-                        <button class="btn-custom-2 increase-cart-item">+</button>
+                        <button class="btn-custom-2 increase-cart-item-btn">+</button>
                     </div>
+                </div>
             </div>`;
+
+        console.log(`Total price of all products is: ${calculateTotalPrice(productPrice)}`)
 
         increaseCounterOfProduct();
         decreaseCounterOfProduct();
     }
 
+    function calculateTotalPrice(price) {
+        return countFinalResult += price;
+    }
+
+    function getIdProductInCart(targetElement) {
+        const myParentElementID = parseInt(targetElement.parentNode.parentNode.parentNode.id.split("-cart-")[1]);
+        return productsInCart.filter((el) => el.id === myParentElementID);
+    }
+
     const increaseCounterOfProduct = function () {
-        document.querySelectorAll(".increase-cart-item").forEach((el) => {
+        document.querySelectorAll(".increase-cart-item-btn").forEach((el) => {
             el.addEventListener(("click"), (e) => {
+                e.stopPropagation();
+
+                e.currentTarget.previousElementSibling.previousElementSibling.disabled = false;
+
+                const returnedObj = getIdProductInCart(e.currentTarget);
+
                 const myPrevElement = getSibling(e.currentTarget, "prev");
                 let counter = parseInt(myPrevElement.textContent).toString();
                 ++counter;
+
+                countFinalResult += parseInt(returnedObj[0].productPrice);
+                console.log(`Final Result: ${countFinalResult}`)
+                setTotalPrice();
+
                 myPrevElement.innerHTML = counter.toString();
             });
         });
     }
 
     const decreaseCounterOfProduct = function () {
-        document.querySelectorAll(".decrease-cart-item").forEach((el) => {
+        document.querySelectorAll(".decrease-cart-item-btn").forEach((el) => {
             el.addEventListener(("click"), (e) => {
+                e.stopPropagation();
+
                 const myNextElement = getSibling(e.currentTarget, "next");
+                const returnedObj = getIdProductInCart(e.currentTarget);
+
                 let counter = parseInt(myNextElement.textContent).toString();
                 --counter;
 
-                if (counter < 1) counter = 1;
+                if (counter <= 1) {
+                    counter = 1;
+                    e.currentTarget.disabled = true;
+                } else {
+                    e.currentTarget.disabled = false;
+                }
+
+                countFinalResult -= parseInt(returnedObj[0].productPrice);
+                console.log(`Final Result: ${countFinalResult}`)
+                setTotalPrice();
 
                 myNextElement.innerHTML = counter.toString();
             });
         });
+    }
+
+    function setTotalPrice() {
+        document.getElementById("total-price").innerHTML = `Total price: ${countFinalResult}`;
     }
 
     function getSibling(element = null ?? "", placeELement = "") {
